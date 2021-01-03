faacc36af067e3c0bea7d7af2113e88b88815601
Usman Ali <usmanali.3099@gmail.com>
2021-01-03 16:40:23 +0500

    Task 2 is completed

--- covidanalyzer.py
+++ covidanalyzer.py
@@ -12,7 +12,7 @@ def total_recovered_ratio(country):
         csv_reader = csv.reader(csv_file)
 
         for line in csv_reader:
-            if line[0] == country.title():
+            if line[0] == country:
 
                 total_cases = int(line[1])
                 total_recovered = int(line[5])
@@ -29,20 +29,48 @@ def total_recovered_ratio(country):
     return 'You have entered the wrong country name!'
 
 
+def average_death_rate(safety_measure):
+    count = 0
+    countries_list = []
+    total_cases = None
+    total_deaths = None
+    countries_death_rate = []
+    result = None
+
+    with open('covid_safety_measures.csv', 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for line in csv_reader:
+            if line[7] == safety_measure:
+                with open('covid_cases_stats.csv', 'r', encoding='utf-8') as csv_new_file:
+                    csv_new_reader = csv.reader(csv_new_file)
+
+                    for new_line in csv_new_reader:
+                        if new_line[0] == line[1]:
+                            if new_line[0] not in countries_list:
+                                countries_list.append(new_line[0])
+                                total_deaths = int(new_line[3].strip() or 0)
+                                total_cases = int(new_line[1])
+                                countries_death_rate.append((total_deaths / total_cases) * 100)
+                                count += 1
+
+    result = sum(countries_death_rate) / count
+    result = "{:.4f}".format(result)
+    return f'{result}% death average found in {count} countries.'
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('task', help='Enter a, b or c to run task 1, 2 or 3 respectively!')
-    parser.add_argument('second_argument', help='Enter Country Name or Safety Measure', nargs='+')
+    parser.add_argument('-a', help='Enter country Name', nargs='+')
+    parser.add_argument('-b', help='Enter Safety Measure', nargs='+')
+    parser.add_argument('c', action='store_true')
 
     args = parser.parse_args()
-    arg_str = ' '.join(args.second_argument)
 
-    if args.task == 'a':
-        if args.second_argument:
-            print(total_recovered_ratio(arg_str))
-    elif args.task == 'b':
-        print()
-    elif args.task == 'c':
-        print()
-    else:
-        print('You have to Enter a, b or c as your first argument to run task 1, 2 or 3 respectively!')
+    if args.a:
+        arg_str = ' '.join(args.a)
+        print(total_recovered_ratio(arg_str.title()))
+
+    elif args.b:
+        arg_str = ' '.join(args.b)
+        print(average_death_rate(arg_str.capitalize()))
