35bf6772da7ad252d726e7370ba0b0675f3914b4
Usman Ali <usmanali.3099@gmail.com>
2021-01-03 23:17:55 +0500

    Task3 is also completed and GUI is also used

--- covidanalyzer.py
+++ covidanalyzer.py
@@ -1,8 +1,9 @@
 import csv
 import argparse
 import sys
-
+from tkinter import *
 ratio = None
+countries_list = []
 
 
 def total_recovered_ratio(country):
@@ -29,9 +30,9 @@ def total_recovered_ratio(country):
     return 'You have entered the wrong country name!'
 
 
-def average_death_rate(safety_measure):
+def average_death_rate(safety_measure, check):
+    global countries_list
     count = 0
-    countries_list = []
     total_cases = None
     total_deaths = None
     countries_death_rate = []
@@ -53,24 +54,65 @@ def average_death_rate(safety_measure):
                                 total_cases = int(new_line[1])
                                 countries_death_rate.append((total_deaths / total_cases) * 100)
                                 count += 1
+    if check == 1:
+        return countries_list
+    else:
+        result = sum(countries_death_rate) / count
+        result = '{:.4f}'.format(result)
+        countries_list.clear()
+        return f'{result}% death average found in {count} countries.'
 
-    result = sum(countries_death_rate) / count
-    result = "{:.4f}".format(result)
-    return f'{result}% death average found in {count} countries.'
+
+def calculate_efficiency(safety_items):
+    global countries_list
+    total_recovered = 0
+    total_cases = 0
+    efficiency = 0
+    countries_name = []
+    safety_measures_efficiencies = []
+
+    for i in range(5):
+        countries_name = average_death_rate(safety_items[i], 1)
+        with open('covid_cases_stats.csv', 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file)
+
+            for line in csv_reader:
+                if line[0] in countries_name:
+                    total_cases += int(line[1])
+                    total_recovered += int(line[5].strip() or 0)
+
+        efficiency = total_recovered / total_cases
+        efficiency = '{:.4f}'.format(efficiency)
+        safety_measures_efficiencies.append(efficiency)
+    root = Tk()
+    x = Label(root, text='Efficiencies of 5 mostly adopted Safety Measures. ')
+    x.pack()
+    for i in range(5):
+        x = Label(root, text=f'{safety_items[i]}: {safety_measures_efficiencies[i]}')
+        x.pack()
+    root.mainloop()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', help='Enter country Name', nargs='+')
     parser.add_argument('-b', help='Enter Safety Measure', nargs='+')
-    parser.add_argument('c', action='store_true')
+    parser.add_argument('-c', action='store_true')
 
     args = parser.parse_args()
-
+    list_items = ['Economic measures', 'Limit public gatherings',
+                  'Introduction of quarantine policies', 'Strengthening the public health system',
+                  'International flights suspension'
+                  ]
     if args.a:
         arg_str = ' '.join(args.a)
         print(total_recovered_ratio(arg_str.title()))
 
     elif args.b:
         arg_str = ' '.join(args.b)
-        print(average_death_rate(arg_str.capitalize()))
+        print(average_death_rate(arg_str.capitalize(), 0))
+
+    elif args.c:
+        calculate_efficiency(list_items)
+    else:
+        print('You have enter wrong command, Kindly Choose command from -a, -b, -c')
